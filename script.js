00373002d455d31475487166800330597654fff6
kpatel7c8 <kpatel7c8@gmail.com>
2022-02-17 01:49:04 -0600

    add form input to new card, inputs are stored in

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+let myWatchlist = [];
+
 const container = document.getElementById('container');
 const buttonCard = document.getElementById('buttonCard');
-buttonCard.addEventListener('click', addCard);
+buttonCard.addEventListener('click', createCard);
 
-function addCard() {
+function createCard() {
   const newCard = document.createElement('div');
-  const newTitle = document.createElement('div');
-  const newDirector = document.createElement('div');
-  const newYear = document.createElement('div');
+  const newTitle = document.createElement('textarea');
+  const newDirector = document.createElement('textarea');
+  const newYear = document.createElement('input');
   const newCardActions = document.createElement('div');
   const newEye = document.createElement('i');
   const newCheck = document.createElement('i');
@@ -21,9 +23,9 @@ function addCard() {
   newCheck.className = 'fa-solid fa-check fa-lg';
   newX.className = 'fa-solid fa-x';
 
-  newTitle.innerHTML = 'Title';
-  newDirector.innerHTML = 'Director';
-  newYear.innerHTML = 'Year';
+  newTitle.id = 'title';
+  newDirector.id = 'director';
+  newYear.id = 'year';
 
   container.insertBefore(newCard, buttonCard);
   newCard.append(newTitle);
@@ -33,10 +35,38 @@ function addCard() {
   newCardActions.append(newEye);
   newCardActions.append(newCheck);
   newCardActions.append(newX);
+
+  newTitle.placeholder = 'Title';
+  newDirector.placeholder = 'Director';
+  newYear.placeholder = 'Year';
+
+  function Movie(title, director, year) {
+    this.title = title;
+    this.director = director;
+    this.year = year;
+  }
+
+  newCheck.addEventListener('click', addMovie);
+  function addMovie() {
+    let newMovie = new Movie(document.getElementById('title').value, document.getElementById('director').value, document.getElementById('year').value);
+  /////////////////////////
+
+    myWatchlist.push(newMovie);
+    console.log(myWatchlist);
+    newCheck.removeEventListener('click',addMovie);
+    console.log(myWatchlist[0].title);
+
+  }
+
 }
 
+//////////////////
 
-////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////
+////  MAKE MOVIE OBJECTS FROM USER INPUT, STORE THEM IN AN ARRAY  ////
+//////////////////////////////////////////////////////////////////////
 
 
 const Othello = new Book('Othello', 'Shakespeare', 221, 'read');
